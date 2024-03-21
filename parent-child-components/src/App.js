f027882884c13e83666831b5c1626d4e3fc42f6f
prem9900 <nagaprem180@gmail.com>
2024-03-21 17:05:26 +0530

    Here is the parent and child components code

---
+++ parent-child-components/src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import ChildComponent from './components/ChildComponent';
+import ParentComponent from './components/ParentComponent';
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>Parent-Child Component Relationship</h1>
+      <ParentComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ parent-child-components/src/components/ChildComponent.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function ChildComponent() {
+  return (
+    <div>
+      <h3>Child Component</h3>
+      <p>This is the child component.</p>
+    </div>
+  );
+}
+
+export default ChildComponent;
