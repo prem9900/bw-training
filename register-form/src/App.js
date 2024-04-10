180d284d902e26661268849d2008906a2b4a187c
prem9900 <nagaprem180@gmail.com>
2024-04-10 17:30:45 +0530

    register-form

---
+++ register-form/src/App.js
@@ -0,0 +1,12 @@
+import Register from "./components/Register";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Register></Register>
+      </header>
+    </div>
+  );
+};
+export default App;
